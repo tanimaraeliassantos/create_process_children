ea164b5baf50ee443ee1ff24650aace155075e04
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2023-10-18 11:56:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,18 +6,8 @@
 /// @brief 
 /// @param  
 /// @return 
-int main(void) {
-   pid_t processId;
-   if ((processId = fork()) ==0) {
-    char app[] = "/bin/echo";
-    char * const argv[] = {app, "success", NULL};
-    if (execv(app, argv) < 0) {
-        perror("execv error");
-    }
-    } else if (processId < 0) {
-        perror("fork error");
-        } else {
-            return EXIT_SUCCESS;
-        }
-        return EXIT_FAILURE;
+int main() {
+        fork();
+        fork();
+        printf("Hello World.\n");
     }
